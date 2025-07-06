0c6b77d32d9bd345304f441c00d8251e1594f5e5
clntsf <colin.tsf@gmail.com>
2025-07-06 18:40:18 +0100

    initial setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as reader:
+    long_description = reader.read()
+
+REQ_PKGS = []
+
+setuptools.setup(
+    name="pytest_lite",
+    version="0.0.1",
+    author="Colin Simon-Fellowes",
+    author_email="colin.tsf@gmail.com",
+    description="Lightweight testing library for python scripts",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/clntsf/pytest_lite",
+    project_urls={
+        "Repo":"https://github.com/clntsf/pytest_lite"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[REQ_PKGS],
+    python_requires=">=3.12.2",
+)
